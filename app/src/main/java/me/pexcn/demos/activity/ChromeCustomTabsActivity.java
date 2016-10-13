b2cad743601f26c07023bf47ef5f12d4a3e8455d
pexcn <pexcn97@gmail.com>
2016-10-13 22:05:21 +0800

    Update

--- app/src/main/java/me/pexcn/demos/activity/ChromeCustomTabsActivity.java
+++ app/src/main/java/me/pexcn/demos/activity/ChromeCustomTabsActivity.java
@@ -1,11 +1,9 @@
 package me.pexcn.demos.activity;
 
-import android.annotation.TargetApi;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Build;
 import android.support.customtabs.CustomTabsIntent;
 import android.widget.Button;
 
@@ -28,17 +26,17 @@ public class ChromeCustomTabsActivity extends BaseActivity {
         return true;
     }
 
-    @TargetApi(Build.VERSION_CODES.M)
+    @SuppressWarnings("deprecation")
     @Override
     protected void init() {
         super.init();
         mButton = (Button) findViewById(R.id.open);
         mButton.setOnClickListener(v -> {
             CustomTabsIntent customTabsIntent = new CustomTabsIntent.Builder()
-                    .setToolbarColor(getResources().getColor(R.color.colorPrimary, null))
+                    .setToolbarColor(getResources().getColor(R.color.colorPrimary))
                     .setShowTitle(true)
                     .addMenuItem("Menus", PendingIntent.getActivity(this, 1, new Intent(), 0))
-                    .setActionButton(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_crop), "Test", PendingIntent.getActivity(this, 2, new Intent(), 0))
+                    .setActionButton(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_crop), "Test文字", PendingIntent.getActivity(this, 2, new Intent(), 0))
                     .addDefaultShareMenuItem()
                     .build();
             customTabsIntent.intent.setPackage("com.android.chrome");
