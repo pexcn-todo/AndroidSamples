6b530c8c48814e27111a95f3b6945efcfcc4f612
pexcn <pexcn97@gmail.com>
2016-10-20 19:32:42 +0800

    Add a activity

---
+++ app/src/main/java/me/pexcn/demos/activity/ListNestedScrollActivity.java
@@ -0,0 +1,15 @@
+package me.pexcn.demos.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import me.pexcn.demos.R;
+
+public class ListNestedScrollActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_nested_scroll);
+    }
+}
